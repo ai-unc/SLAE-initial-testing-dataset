e26d71495b127187978b482e90b9d1f6bb0dc937
Hanqi Xiao <hanqixiao.personal@gmail.com>
2023-10-21 17:02:46 -0400

    fixed some formatting issues

--- pipelines/single_variable_pipeline.py
+++ pipelines/single_variable_pipeline.py
@@ -30,6 +30,7 @@ class SingleRelation(BaseModel):
     VariableOneName: str
     VariableTwoName: str
     RelationshipClassification: str
+    isCausal_True_or_False: str
     SupportingText: str
 
     @validator("RelationshipClassification")
@@ -39,30 +40,22 @@ class SingleRelation(BaseModel):
         else:
             raise ValueError(f"Invalid Relationship Type {{{field}}}")
 
-def extract_relationships(text, variable_one, variable_two):
+def extract_relationships(text, variable_one, variable_two, verbose = False):
     # Add map reduce or some other type of summarization function here.
     processed_text = text
 
     # Create Parser
     parser = PydanticOutputParser(pydantic_object=SingleRelation) #Refers to a class called SingleRelation
 
-    # Create the plain text prompt, using some of langchain's functions to automatically create formatting prompts.
+    # Create the plain text prompt. Used some of langchain's functions to automatically create formated prompts.
     query= f"""Given the text, identify the relationship between {variable_one} 
         and {variable_two}. The relationship can only be 'direct', 'inverse', 
         or 'inconclusive'. The SupportText field of your output should include a section 
         of verbatim from the text in addition to any comments you want to make about your output.
         Use exactly wording of outputs choices and input variable names, including capitalization.
         """
-        # The format of the output should 
-        # be a json of the form: 
-        # {{
-        # "VariableOneName": "variable_one",
-        # "VariableTwoName": "variable_two",
-        # "RelationshipClassification": "relationship",
-        # "SupportingText": "supporting_text"
-        # }}
     prompt = PromptTemplate(
-        template = "{text}\n\n{query}\n\n{format_instructions}\n\n",
+        template = """{text}\n\n{query}\n\n{format_instructions}\n\n""",
         input_variables=["query", "text"],
         partial_variables={"format_instructions": parser.get_format_instructions()},
     )
@@ -72,15 +65,22 @@ def extract_relationships(text, variable_one, variable_two):
         f.write(input_text)
         f.write("\n\n\n")
     human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
-    print("what is human_message_prompt:", type(human_message_prompt))
+    if verbose:
+        print("what is human_message_prompt:", type(human_message_prompt))
     chat_prompt = ChatPromptTemplate.from_messages([human_message_prompt])
-    print("what is chat_prompt:", type(chat_prompt))
+    if verbose:
+        print("what is chat_prompt:", type(chat_prompt))
+    completion_prompt = chat_prompt.format_prompt(query=query, text=processed_text).to_messages()
+    if verbose:
+        print("What is a completion_prompt:", type(completion_prompt))
 
-    # Get an output from the LLM
+    # Create LLM
     model = ChatOpenAI(temperature=.3, openai_api_key=key, model_name="gpt-3.5-turbo-16k")
-    print("What is a chat_prompt.format_prompt().to_messages():", type(chat_prompt.format_prompt(query=query, text=processed_text).to_messages()[0]))
-    output = model(chat_prompt.format_prompt(query=query, text=processed_text).to_messages())
-    print("what is a output:", type(output), output.content)
+
+    # Obtain completion from LLM
+    output = model(completion_prompt)
+    if verbose:
+        print("what is a output:", type(output), output.content)
     with open(OUTPUTS_SOURCE / "SingleVariablePipelineOutput.txt", "a") as f:
         f.write("pre parse: ")
         f.write(str(output.content))
@@ -88,13 +88,14 @@ def extract_relationships(text, variable_one, variable_two):
     output = parser.parse(output.content)
     return output
 
-    # Return None if no relationship was identified
-
-# Usage:
-with open(PAPER_SOURCE / "EpidemiologyandEtiologyofSubstance_10.3109_00952990.2012.694527.txt") as f:
+# Prepare inputs:
+text = str()
+with open(PAPER_SOURCE / "testpaper.txt") as f:
     text = f.read()
-variable_one = "Demographic"
-variable_two = "epidemiological patterns of substance use"
+variable_one = "AI/AN status"
+variable_two = "Substance use"
+
+# Process and save outputs:
 output = extract_relationships(text, variable_one, variable_two)
 with open(OUTPUTS_SOURCE / "SingleVariablePipelineOutput.txt", "a") as f:
     f.write("successful parse: ")
