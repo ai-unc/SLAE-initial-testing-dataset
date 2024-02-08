bace3319260c63c646e211c6e6b34ec9bfdc1575
Hanqi Xiao <hanqixiao.personal@gmail.com>
2024-02-08 18:18:34 -0500

    control flow

---
+++ evaluations/flow_control.py
@@ -0,0 +1,13 @@
+# Obtain list of relations from Kumu or Vensim file
+
+# Use the matcher to separate the list of relations, resulting in a series of python dictionaries each with a single paper and a list of related relations.
+
+# Obtain settings by reading in the file
+
+# For each dictionary run the "extract_relationships" function and settings
+
+# Obtain a list of output jsons from the iterative running of extract_relationships
+
+# Recombine the output jsons into the format required to output to Kumu
+
+# Parse it into Kumu form and save the json in a file. 
