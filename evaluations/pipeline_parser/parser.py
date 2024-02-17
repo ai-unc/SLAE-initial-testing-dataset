602471a160aebd1fd6022156dcbe393f97e822b9
lil_cosine <baeden.vester@gmail.com>
2024-02-17 12:23:39 -0500

    pipeline to kumu parser done(?)

--- evaluations/pipeline_parser/parser.py
+++ evaluations/pipeline_parser/parser.py
@@ -7,37 +7,42 @@ def pipeline_to_kumu(dic, outputPath):
     connections = []
     elementList = []
     connectionList = []
-
+    verdicts = {}
     # Process each JSON object in the list
 
-    # for paper in dic["Papers"]:
-    paper = dic
-    title = paper['PaperTitle']
-    doi = paper['PaperDOI']
+    for paper in dic["Papers"]:
+        title = paper['PaperTitle']
+        doi = paper['PaperDOI']
 
-    # Extract the variables and connections from each relation in the JSON object
-    for relation in paper['Relations']:
-        variable_one = relation['VariableOneName']
-        variable_two = relation['VariableTwoName']
-        relationType = relation['RelationshipClassification']
-        SupportingText = relation['SupportingText']
-        if "isCausal" in relation:
-            if relation['isCausal'] == "True":
-                relationType = "causal"
-            elif relation['isCausal'] == "False":
-                relationType = "non-causal"
-        else:
-            relationType = ""
-        # Add the variables to the list if they are not already present
-        if(variable_one not in vars):
-            vars.append(variable_one)
-        if(variable_two not in vars):
-            vars.append(variable_two)
+        # Extract the variables and connections from each relation in the JSON object
+        for relation in paper['Relations']:
+            variable_one = relation['VariableOneName']
+            variable_two = relation['VariableTwoName']
+            relationType = relation['RelationshipClassification']
+            SupportingText = relation['SupportingText']
+            if "isCausal" in relation:
+                if relation['isCausal'] == "True":
+                    isCausal = "causal"
+                elif relation['isCausal'] == "False":
+                    isCausal = "non-causal"
+            else:
+                isCausal = ""
+            # Add the variables to the list if they are not already present
+            if(variable_one not in vars):
+                vars.append(variable_one)
+            if(variable_two not in vars):
+                vars.append(variable_two)
             
-        connections.append([variable_one, variable_two, relationType, SupportingText, relationType])
+            if (variable_one, variable_two) not in verdicts:
+                verdicts[(variable_one,variable_two)] = []
+                verdicts[(variable_one,variable_two)].append( {"title": title, "doi": doi, "relationType": relationType, "isCausal": isCausal, "SupportingText": SupportingText})
+            else:
+                verdicts[(variable_one,variable_two)].append({"title": title, "doi": doi, "relationType": relationType, "isCausal": isCausal, "SupportingText": SupportingText})
+
+            connections.append([variable_one, variable_two])
 
             
-
+    #print(verdicts)
     # Create the element list for the output JSON
     for var in vars:
         entry = {
@@ -49,16 +54,17 @@ def pipeline_to_kumu(dic, outputPath):
     # Create the connection list for the output JSON
 
     for connection in connections:
-        if connection[2] == "direct" or connection[2] == "Direct":
-            connection[2] = "directed"
-        else:
-            connection[2] = "mutual"
+        # if connection[2] == "direct" or connection[2] == "Direct":
+        #     connection[2] = "directed"
+        # else:
+        #     connection[2] = "mutual"
+    
         entry = {
             "from": connection[0],
             "to": connection[1],
-            "direction": connection[2],
-            "type": connection[4],
-            "description": connection[3]
+            #"direction": connection[2],
+            #"type": connection[4],
+            "description": '\n=====\n'.join([str(i) for i in verdicts[(connection[0], connection[1])]])
         }
         connectionList.append(entry)
 
