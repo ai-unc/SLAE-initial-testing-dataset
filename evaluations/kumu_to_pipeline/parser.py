f296e292bff0e36a7ea5d856d14daf3831973def
lil_cosine <baeden.vester@gmail.com>
2024-02-21 13:18:18 -0500

    kumu to pipeline fixes

--- evaluations/kumu_to_pipeline/parser.py
+++ evaluations/kumu_to_pipeline/parser.py
@@ -1,27 +1,32 @@
 import json
+import ast
 
 def kumu_to_pipeline(file):
-    curFile = json.load(open("./inputs/" + file))
+    curFile = json.load(open("./evaluations/kumu_to_pipeline/inputs/" + file))
     output = {
         "relations": []
     }
     
 
     for connection in curFile['connections']:
-        if connection["direction"] == "directed":
-            connection["direction"] = "direct"
-
-        if connection["type"] == "causal":
-            connection["type"] = "True"
-        elif connection["type"] == "non-causal":
-            connection["type"] = "False"
+        consolidate = {
+            "relationshipc": [],
+            "iscausal": [],
+            "supportingtext": [],
+        }
+        attrs = connection['description'].split("\n=====\n")
+        for attr in attrs:
+            temp = ast.literal_eval(attr)
+            consolidate['relationshipc'].append(temp['relationType'])
+            consolidate['iscausal'].append(temp['isCausal'])
+            consolidate['supportingtext'].append(temp['SupportingText'])
 
         entry = {
             "VariableOneName": connection['from'],
             "VariableTwoName": connection['to'],
-            "RelationshipClassification": connection['direction'],
-            "isCausal" : connection['type'],
-            "SupportingText": connection['description']
+            "RelationshipClassification": consolidate['relationshipc'],
+            "isCausal" : consolidate['iscausal'],
+            "SupportingText": consolidate['supportingtext']
         }
         output['relations'].append(entry)
     with open("./outputs/" + file[:-5] + "_output.json", "w") as f:
@@ -35,22 +40,28 @@ def kumu_to_pipeline_no_io(dictionary):
     
 
     for connection in curFile['connections']:
-        if connection["direction"] == "directed":
-            connection["direction"] = "direct"
-
-        if connection["type"] == "causal":
-            connection["type"] = "True"
-        elif connection["type"] == "non-causal":
-            connection["type"] = "False"
+        consolidate = {
+            "relationshipc": [],
+            "iscausal": [],
+            "supportingtext": [],
+        }
+        attrs = connection['description'].split("\n=====\n")
+        for attr in attrs:
+            temp = ast.literal_eval(attr)
+            consolidate['relationshipc'].append(temp['relationType'])
+            consolidate['iscausal'].append(temp['isCausal'])
+            consolidate['supportingtext'].append(temp['SupportingText'])
 
         entry = {
             "VariableOneName": connection['from'],
             "VariableTwoName": connection['to'],
-            "RelationshipClassification": connection['direction'],
-            "isCausal" : connection['type'],
-            "SupportingText": connection['description']
+            "RelationshipClassification": consolidate['relationshipc'],
+            "isCausal" : consolidate['iscausal'],
+            "SupportingText": consolidate['supportingtext']
         }
         output['relations'].append(entry)
     
     return output
 
+if __name__ == "__main__":
+    kumu_to_pipeline("kumu_test.json")
